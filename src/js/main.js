a9d3a491885f6cd5a9720b5720aa0befe6c445f8
zedturner <zdturner94@gmail.com>
2022-03-31 11:46:07 -0400

    formatted menus and added js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
+var button = document.querySelector(".show_hide");
+var infomenu = document.querySelector(".article_info");
+var hexbutton = document.querySelector(".nav_hex");
+var hexmenu = document.querySelector(".hex_menu");
+
+function toggle() {
+  infomenu.classList.toggle("active");
+  button.classList.toggle("active");
+  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("data-text-original");
+  } else {
+    button.setAttribute("data-text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("data-text-swap");
+  };
+}
+
+function hextoggle() {
+    hexmenu.classList.toggle("active");
+    hexbutton.classList.toggle("active");
+}
+
+button.addEventListener("click", toggle);
+hexbutton.addEventListener("click", hextoggle);
+
+
 
