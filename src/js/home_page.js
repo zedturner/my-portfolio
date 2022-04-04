b3d44d4f3fbc343de82480a0eb225b400bfd5394
zedturner <zdturner94@gmail.com>
2022-04-04 14:39:24 -0400

    added parallax css and java to home page

---
+++ src/js/home_page.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+function App() {
+    return ( 
+        <section className="App"> 
+            <h1>Hello world</h1>
+        </section>
+    ); 
+}
+
+
+export default App; 
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,12 @@ var infomenu = document.querySelector(".article_info");
 var hexbutton = document.querySelector(".nav_hex");
 var hexmenu = document.querySelector(".hex_menu");
 
+const parallax1 = document.getElementById("foreground"); 
+const parallax2 = document.getElementById("logo");
+const parallax3 = document.getElementById("midgard");
+const parallax4 = document.getElementById("highgard");
+const parallax5 = document.getElementById("background");
+
 function toggle() {
   infomenu.classList.toggle("active");
   button.classList.toggle("active");
@@ -19,8 +25,16 @@ function hextoggle() {
     hexbutton.classList.toggle("active");
 }
 
+window.addEventListener("scroll", function()
+{
+  let offset = window.pageYOffset; 
+  parallax1.style.bottom = -1200 + offset * 1.2 + "px";
+  parallax2.style.top = offset * 1.6 + "px"; 
+  parallax2.style.opacity = 100 - offset * 0.8 + "%";
+  parallax3.style.bottom = -1200 + offset * 1.0 + "px"; 
+  parallax4.style.bottom = -1200 + offset * 0.6 + "px"; 
+  parallax5.style.bottom = -1200 + offset * 0.4 + "px";
+})
+
 button.addEventListener("click", toggle);
 hexbutton.addEventListener("click", hextoggle);
-
-
-
